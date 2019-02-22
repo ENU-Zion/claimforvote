70908d8b07b702cbb8282f3bbc9d78eec226649c
qiushaoxi <qiushaoxi@163.com>
2019-02-22 16:38:40 +0800

    change to production

--- claimforvote.hpp
+++ claimforvote.hpp
@@ -5,15 +5,14 @@
 #include <enulib/crypto.h>
 #include "enu.token.hpp"
 
-#define TOKEN_CONTRACT N(t.red.test.v) //need to change
-#define TOKEN_SYMBOL S(4, VOTE) //need to change
+#define TOKEN_CONTRACT N(ltsonenumivo)
+#define TOKEN_SYMBOL S(4, LTS)
 #define LOTTERY_POOL N(claimlottery)
-#define LOTTERY_RATE_PERCENT 50 //need to change
+#define LOTTERY_RATE_PERCENT 10
 #define LOTTERY_THRESHOLD 500 * 10000
 #define LOTTERY_AMOUNT 10000
 #define LOTTERY_TOKEN_CONTRACT N(enu.token)
 #define LOTTERY_TOKEN_SYMBOL S(4, ENU)
-
 
 using namespace enumivo;
 using namespace std;
